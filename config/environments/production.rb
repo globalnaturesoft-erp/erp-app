b76b5f15d1dfbc46f21347d028e5a71d23c5c1dd
Luan Pham <luanpm88@gmail.com>
2017-05-23 01:28:29 -0700

    change production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'timhangcongnghe.vn' }
+
+  # set delivery method to :smtp, :sendmail or :test
+  config.action_mailer.delivery_method = :smtp
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'globalnaturesoft.com',
+    user_name: 'soft.support@hoangkhang.com.vn',
+    password: 'aA456321@#$',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
