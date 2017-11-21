d4bf264ad827a94f1c9816267662177fa3ba0dcc
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-11-21 22:31:59 +0700

    fix main app

--- db/schema.rb
+++ db/schema.rb
@@ -10,46 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503020426) do
+ActiveRecord::Schema.define(version: 20171116080404) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "erp_editor_uploads", id: :serial, force: :cascade do |t|
+  create_table "erp_editor_uploads", force: :cascade do |t|
     t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "erp_users", id: :serial, force: :cascade do |t|
+  create_table "erp_user_groups", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "permissions"
+  end
+
+  create_table "erp_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
+    t.integer "sign_in_count", precision: 38, default: 0, null: false
+    t.datetime "current_sign_in_at", precision: 6
+    t.datetime "last_sign_in_at", precision: 6
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "provider"
     t.string "uid"
     t.boolean "backend_access", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "avatar"
     t.string "timezone"
     t.boolean "active", default: false
-    t.integer "creator_id"
+    t.integer "creator_id", precision: 38
     t.text "permissions"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.index ["confirmation_token"], name: "index_erp_users_on_confirmation_token", unique: true
+    t.datetime "confirmed_at", precision: 6
+    t.datetime "confirmation_sent_at", precision: 6
+    t.integer "user_group_id", precision: 38
+    t.index ["confirmation_token"], name: "i_erp_users_confirmation_token", unique: true
     t.index ["creator_id"], name: "index_erp_users_on_creator_id"
     t.index ["email"], name: "index_erp_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_erp_users_on_reset_password_token", unique: true
+    t.index ["reset_password_token"], name: "i_erp_use_res_pas_tok", unique: true
+    t.index ["user_group_id"], name: "i_erp_users_user_group_id"
   end
 
 end
